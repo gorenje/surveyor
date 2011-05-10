7ce9e490b8a6a167a8b01da45351a27168468c56
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-05-10 17:30:40 +0200

    display result only if it's an answer to the current question

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,16 +1,17 @@
 module ResultsHelper
   def display_response(r_set, question)
-    sets = r_set.responses.select { |r| r.question.display_order == question.display_order }
+    sets = r_set.responses.select do |r|
+      r.question == question && r.question.display_order == question.display_order
+    end
+
     if sets.size == 0
       "-"
     elsif sets.size == 1
       (sets.first.string_value || sets.first.text_value || show_answer(sets.first))
     else
-      txt = ""
-      sets.each do |set|
-        txt << show_answer(set) + "<br/>"
-      end
-      txt
+      sets.collect do |set|
+        show_answer(set) + "<br/>"
+      end.join
     end
   end
 
