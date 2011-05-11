25038afec6b3c7f142a056b211f2c9706f1f4d39
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-05-11 18:35:54 +0200

    handle saving string values correctly. Check for array first and then continue, this was stopping simple string fields from being saved

--- lib/surveyor/models/response_methods.rb
+++ lib/surveyor/models/response_methods.rb
@@ -10,19 +10,25 @@ module Surveyor
         unless @@validations_already_included
           # Validations
           base.send :validates_presence_of, :response_set_id, :question_id, :answer_id
-          
+
           @@validations_already_included = true
         end
         base.send :include, Surveyor::ActsAsResponse # includes "as" instance method
-        
+
         # Class methods
         base.instance_eval do
           def applicable_attributes(attrs)
             result = HashWithIndifferentAccess.new(attrs)
-            result[:answer_id] = result[:answer_id].compact.delete_if(&:blank?) if result[:answer_id].is_a?(Array)
-            if result[:answer_id].present? && result[:string_value] && Answer.exists?(result[:answer_id])
+            result[:answer_id] = result[:answer_id].
+              compact.delete_if(&:blank?) if result[:answer_id].is_a?(Array)
+
+            if (result[:answer_id].present? && result[:string_value] &&
+                Answer.exists?(result[:answer_id]))
               answer = Answer.find(result[:answer_id])
-              result.delete(:string_value) unless answer.is_a?(Array)&&answer.response_class && answer.response_class.to_sym == :string
+              unless answer.is_a?(Array)
+                result.delete(:string_value) unless (answer.response_class &&
+                                                     answer.response_class.to_sym == :string)
+              end
             end
             result
           end
