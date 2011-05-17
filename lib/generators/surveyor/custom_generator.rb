9dad3510bbc660b2c9f272c4dae4b31a0134f27f
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-05-17 13:30:02 +0200

    replaced erb template with haml template for the extending surveyor generator

--- lib/generators/surveyor/custom_generator.rb
+++ lib/generators/surveyor/custom_generator.rb
@@ -2,7 +2,7 @@ require 'rails/generators'
 module Surveyor
   class CustomGenerator < Rails::Generators::Base
     source_root File.expand_path("../templates", __FILE__)
-    
+
     def readme
       copy_file "surveys/EXTENDING_SURVEYOR.md"
     end
@@ -10,8 +10,7 @@ module Surveyor
       copy_file "app/controllers/surveyor_controller.rb"
     end
     def layout
-      copy_file "app/views/layouts/surveyor_custom.html.erb"
+      copy_file "app/views/layouts/surveyor_custom.html.haml"
     end
-    
   end
 end
